a45c4dbfc1d4513c27dbdd3eaf9101820206efa0
Chunni <chunni.deng@gmail.com>
2015-08-26 16:23:28 +0800

    fix path

--- spanparser.go
+++ spanparser.go
@@ -2,7 +2,7 @@ package fipmd
 
 import (
 	"bytes"
-	. "github.com/fipress/fiputil
+	. "github.com/fipress/fiputil"
 )
 
 /*func parsePlainUntil(input []byte, until byte,skipFuncs ...func([]byte) int) (ps []*plain, idx int) {
